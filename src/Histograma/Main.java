1cdb5407b5bf681136886a89f68a9baf208358f5
Entrar <Entrar@10.22.147.47>
2022-09-29 12:17:46 +0100

    First version kata2

---
+++ src/Histograma/Main.java
@@ -0,0 +1,27 @@
+package Histograma;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] data = {4,2,6,4,9,8,0,1,2,6,3,7,8,5};
+        Map<Integer,Integer> histogram = new HashMap<Integer,Integer>();
+        
+        for (int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])){
+                histogram.put(data[i], histogram.get(data[i])+1);
+            } else {
+                histogram.put(data[i], 1);
+            }
+        }
+        
+        for (Map.Entry<Integer,Integer> entry : histogram.entrySet()) {
+            System.out.println(entry.getKey()+" ===> "+entry.getValue());
+        }
+        
+    }
+    
+}
